b80816f71924425f462e9a7ba5ce1829cd162f15
hlaclau <hugo.laclau@gmail.com>
2026-04-16 14:57:44 +0200

    fix: display the username of the loggedin user

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -3,11 +3,11 @@ package domain
 import "time"
 
 type User struct {
-	ID           string    `db:"id"`
-	Username     string    `db:"username"`
-	Email        string    `db:"email"`
-	PasswordHash string    `db:"password_hash"`
-	Bio          *string   `db:"bio"`
-	AvatarURL    *string   `db:"avatar_url"`
-	CreatedAt    time.Time `db:"created_at"`
+	ID           string    `db:"id"            json:"id"`
+	Username     string    `db:"username"      json:"username"`
+	Email        string    `db:"email"         json:"email"`
+	PasswordHash string    `db:"password_hash" json:"-"`
+	Bio          *string   `db:"bio"           json:"bio"`
+	AvatarURL    *string   `db:"avatar_url"    json:"avatar_url"`
+	CreatedAt    time.Time `db:"created_at"    json:"created_at"`
 }
